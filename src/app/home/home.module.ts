82436070db7c9e04720c3fb0115b7fe80dff210d
tazbin <md.tazbinur.rahaman@gmail.com>
2020-08-07 04:43:35 +0600

    project done v1.0

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 
 @NgModule({
@@ -12,7 +13,24 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgCircleProgressModule.forRoot({
+      backgroundPadding: -5,
+      radius: 140,
+      toFixed: 0,
+      maxPercent: 100,
+      imageHeight: 121,
+      imageWidth: 119,
+      showTitle: false,
+      showSubtitle: false,
+      showUnits: false,
+      showBackground: false,
+      startFromZero: false,
+      showZeroOuterStroke: false,
+      outerStrokeWidth: 15,
+      innerStrokeWidth: 5,
+      animationDuration: 0
+    })
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,66 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  restTime: any = '00:00:00';
+  minute: number;
+  second: any;
+  totalTime: number;
+
+  timeSpend: any = {
+    h: 0,
+    m: 0,
+    s: 0
+  };
+
+  goTimer: any = false;
+
+  percent: number = 0;
+  progress: number = 1;
+
   constructor() {}
+
+  startTimer(){
+
+    if( this.goTimer != false ){
+      clearInterval(this.goTimer);
+      this.restTime = '00:00:00';
+      this.timeSpend = {
+        h: 0,
+        m: 0,
+        s: 0
+      };
+      this.percent = 0;
+  this.progress = 1;
+  this.goTimer = false;
+
+    } else{
+
+
+
+      let timeSplit = this.restTime.split(':');
+      this.minute = timeSplit[1];
+      this.second = timeSplit[2];
+      this.totalTime = Math.floor(this.minute*60) + parseInt(this.second);
+    
+    this.goTimer = setInterval(()=>{
+
+      if( this.percent >= 100 ){
+        clearInterval(this.goTimer);
+      } else{
+        this.percent = (this.progress/this.totalTime)*100;
+      this.progress++;
+
+      this.timeSpend.s++;
+      if( this.timeSpend.s > 59 ){
+        this.timeSpend.m++;
+        this.timeSpend.s = 0
+      }
+      }
+    }, 1000)
+
+
+
+    }
+  };
 
 }
